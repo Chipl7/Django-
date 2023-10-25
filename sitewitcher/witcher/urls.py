9473af94c340fe02da2b1f97ff18a4bfd97df850
Artem Dubrovin <129255243+Chipl7@users.noreply.github.com>
2023-10-25 23:42:43 +0700

    Настроена конфигурация проекта. Конвертеры, и создание собственных конвертеров.

--- sitewitcher/witcher/urls.py
+++ sitewitcher/witcher/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index),  # http://127.0.0.1:8000
-    path('witchers_school/', views.categories),  # http://127.0.0.1:8000/witchers_school/
+    path('witchers_school/<int:witcher_school_id>/', views.categories),  # http://127.0.0.1:8000/witchers_school/2/
+    path('witchers_school/<slug:witcher_school_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/witchers_school/asdqwe/
+    path('archive/<year4:year>/', views.archive)
 ]
--- sitewitcher/witcher/views.py
+++ sitewitcher/witcher/views.py
@@ -6,5 +6,13 @@ def index(request):  # HttpRequest
     return HttpResponse('Страница приложения witcher')
 
 
-def categories(request):
-    return HttpResponse('<h1>Ведьмачьи школы</h1>')
+def categories(request, witcher_school_id):
+    return HttpResponse(f'<h1>Ведьмачьи школы</h1><p>id: {witcher_school_id}</p>')
+
+
+def categories_by_slug(request, witcher_school_slug):
+    return HttpResponse(f'<h1>Ведьмачьи школы</h1><p>slug: {witcher_school_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
