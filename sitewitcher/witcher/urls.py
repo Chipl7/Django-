d92fd165d9ef463d5092b52680a66a7fc9221450
Artem Dubrovin <129255243+Chipl7@users.noreply.github.com>
2023-11-01 23:06:18 +0700

    Стандартные шаблонные фильтры. Теги шаблонов. Шаблонный тег url.

--- sitewitcher/witcher/urls.py
+++ sitewitcher/witcher/urls.py
@@ -7,8 +7,9 @@ register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index, name='home'),  # http://127.0.0.1:8000
-    path('about/', views.about, name='about'),  # http://127.0.0.1:8000
-    path('witchers_school/<int:witcher_school_id>/', views.categories, name='witcher_school_id'),  # http://127.0.0.1:8000/witchers_school/2/
-    path('witchers_school/<slug:witcher_school_slug>/', views.categories_by_slug, name='school'),  # http://127.0.0.1:8000/witchers_school/asdqwe/
-    path('archive/<year4:year>/', views.archive, name='archive')
+    path('about/', views.about, name='about'),  # http://127.0.0.1:8000/about/
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name ='post'),
 ]
--- sitewitcher/witcher/views.py
+++ sitewitcher/witcher/views.py
@@ -2,14 +2,20 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
 
-menu = ['о сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'}
+]
 
-
-class Myclass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [
+    {'id': 1, 'title': 'Геральт из Ривии', 'content': 'Биография Геральта', 'is_published': True},
+    {'id': 2, 'title': 'Йеннифэр из Венгерберга', 'content': 'Биография Йеннифэр', 'is_published': False},
+    {'id': 3, 'title': 'Цирилла Фиона Элен Рианнон', 'content': 'Биография Аире', 'is_published': True},
+          ]
 
 
 def index(request):  # HttpRequest
@@ -18,11 +24,7 @@ def index(request):  # HttpRequest
     data = {
             'title': 'Главная страница',
             'menu': menu,
-            'float': 28.56,
-            'lst': [1, 2, 'abc', True],
-            'set': {1, 2, 3, 2, 5},
-            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-            'obj': Myclass(10, 20)
+            'posts': data_db,
             }
     return render(request, 'witcher/index.html', context = data)
 
@@ -31,21 +33,20 @@ def about(request):
     return render(request, 'witcher/about.html', {'title': 'О сайте'})
 
 
-def categories(request, witcher_school_id):
-    return HttpResponse(f'<h1>Ведьмачьи школы</h1><p>id: {witcher_school_id}</p>')
+def show_post(request, post_id):
+    return HttpResponse(f'Отображать статью с id = {post_id}')
 
 
-def categories_by_slug(request, witcher_school_slug):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f'<h1>Ведьмачьи школы</h1><p>slug: {witcher_school_slug}</p>')
+def addpage(request):
+    return HttpResponse('Добавить статью')
 
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('school', args=('school', ))
-        return redirect(uri)
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
 
 
 def page_not_found(request, exception):
