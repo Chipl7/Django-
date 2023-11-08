58595788a89b5184737d0d1651d76b4e4abcafca
Artem Dubrovin <129255243+Chipl7@users.noreply.github.com>
2023-11-08 20:44:26 +0700

    Наследование шаблонов(extends). Тег(include). Подключены статические файлы.

--- sitewitcher/sitewitcher/settings.py
+++ sitewitcher/sitewitcher/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=l5n)&e!fd=lzjk!19d9w-3q$_m(em(*s_&$ibj0%c(u-j4gbc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'sitewitcher.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
